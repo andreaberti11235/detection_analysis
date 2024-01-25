655bb52a77b0c583a0170eeda6f3f54c1e7c8b2b
Andrea Berti <andrea.berti11235@icloud.com>
2024-01-25 12:39:18 +0100

    to be tested

--- count_classify_detections.py
+++ count_classify_detections.py
@@ -1,5 +1,7 @@
 import os
 import argparse
+from datetime import datetime
+import glob
 import numpy as np
 from numpy import linalg
 from matplotlib import pyplot as plt
@@ -8,64 +10,97 @@ import pandas as pd
 
 def main():
     parser = argparse.ArgumentParser(description='Analyse the prediction outputs to find TP and FP.')
-    parser.add_argument('gt_txt', help='Absolute path of the txt file with the ground-truth annotations')
-    parser.add_argument('pred_txt', help='Absolute path of the txt file with the annotations of the detected bboxes')
+    parser.add_argument('gt_dir', help='Absolute path of the folder containing the ground-truth txt files')
+    parser.add_argument('pred_dir', help='Absolute path of the folder containing the prediction txt files')
+    parser.add_argument('out_dir', help='Absolute path of the folder where the output file, with the resulting numbers, will be saved')
     args = parser.parse_args()
 
-    gt_txt = args.gt_txt
-    pred_txt = args.pred_txt
+    gt_dir = args.gt_dir
+    pred_dir = args.pred_dir
+    out_dir = args.out_dir
 
-    gt_df = pd.read_csv(gt_txt, sep=' ', header=None)
-    pred_df = pd.read_csv(pred_txt, sep=' ', header=None)
+    tot_n_masses = 0
+    nr_TP = 0
+    nr_FP = 0
+    nr_FN = 0
 
-    gt_list = []
-    pred_list = []
+    for element in glob.glob(os.path.join(gt_dir, '*')):
+        gt_txt = element
+        pred_txt = os.path.join(pred_dir, os.basename(element))
 
-    for idx in gt_df.index:
-        x_center = gt_df.iloc[idx][1]
-        y_center = gt_df.iloc[idx][2]
-        width = gt_df.iloc[idx][3]
-        height = gt_df.iloc[idx][4]
+        gt_df = pd.read_csv(gt_txt, sep=' ', header=None)
+        pred_df = pd.read_csv(pred_txt, sep=' ', header=None)
 
-        gt_element = [x_center, y_center, width, height]
-        gt_list.append(gt_element) # è una lista di liste, ciascuna delle quali contiene gli elementi di un bbox
-
-    for idx in pred_df.index:
-        x_center = pred_df.iloc[idx][1]
-        y_center = pred_df.iloc[idx][2]
-        width = pred_df.iloc[idx][3]
-        height = pred_df.iloc[idx][4]
-        confidence = pred_df.iloc[idx][5]
-
-        pred_element = [x_center, y_center, width, height, confidence]
-        pred_list.append(pred_element)
-
-    for detection in pred_list:
-        distances = []
-        # per ogni elemento trovato, inizializzo una lista, 
-        # poi guardo quanto dista da tutti i bbox della gt e appendo alla lista
-        for bbox in gt_list:
-            pred_xy = np.array(detection[0:2])
-            gt_xy = np.array(bbox[0:2])
-
-            distance = linalg.norm(pred_xy - gt_xy)
-            distances.append(distance)
-        
-        closest_item = np.argmin(distances)
-        diagonal = np.hypot(gt_list[closest_item][2], gt_list[closest_item][3])
-
-        if distances[closest_item] <= (1.5*diagonal)/2:
-            print(f'è un vero Vero Positivo, massa più vicina riga {closest_item}')
-
-        else:
-            print('è un Falso Positivo')
+        gt_list = []
+        pred_list = []
 
 
+        for idx in gt_df.index:
+            x_center = gt_df.iloc[idx][1]
+            y_center = gt_df.iloc[idx][2]
+            width = gt_df.iloc[idx][3]
+            height = gt_df.iloc[idx][4]
 
+            gt_element = [x_center, y_center, width, height]
+            gt_list.append(gt_element) # è una lista di liste, ciascuna delle quali contiene gli elementi di un bbox
 
-    # leggo tutti i GT e li metto in una lista,
-    # leggo tutte le predizioni e le metto in una lista
-    # per ogni predizione dovrei calcolare le distanze da ogni GT
+        found_masses = np.zeros(shape=len(gt_list))
+        tot_n_masses += len(gt_list)
+
+        for idx in pred_df.index:
+            x_center = pred_df.iloc[idx][1]
+            y_center = pred_df.iloc[idx][2]
+            width = pred_df.iloc[idx][3]
+            height = pred_df.iloc[idx][4]
+            confidence = pred_df.iloc[idx][5]
+
+            pred_element = [x_center, y_center, width, height, confidence]
+            pred_list.append(pred_element)
+
+        for detection in pred_list:
+            distances = []
+            # per ogni elemento trovato, inizializzo una lista, 
+            # poi guardo quanto dista da tutti i bbox della gt e appendo alla lista
+            for bbox in gt_list:
+                pred_xy = np.array(detection[0:2])
+                gt_xy = np.array(bbox[0:2])
+
+                distance = linalg.norm(pred_xy - gt_xy)
+                distances.append(distance)
+            
+            closest_item = np.argmin(distances)
+            diagonal = np.hypot(gt_list[closest_item][2], gt_list[closest_item][3])
+
+            if distances[closest_item] <= (1.5*diagonal)/2:
+                # print(f'è un vero Vero Positivo, massa più vicina riga {closest_item}')
+                if found_masses[closest_item] == 1:
+                    print(f'Errore: massa riga {closest_item} del paziente {os.basename(element)} già trovata!')
+                else:
+                    nr_TP += 1
+                found_masses[closest_item] = 1
+                
+
+            else:
+                # print('è un Falso Positivo')
+                nr_FP +=1
+
+        nr_FN += len(found_masses) - np.sum(found_masses)
+
+    now = datetime.now()
+    dt_string = now.strftime("%d_%m_%Y__%H_%M")
+    filename = f'count_{dt_string}.txt'
+   
+    while os.path.exists(os.path.join(out_dir, filename)):
+        filename = f'{filename[:-4]}_new.txt'
+    
+    with open(os.path.join(out_dir, filename), 'w') as out_file:
+        out_file.write(f'Total number of found masses TP={nr_TP}\n')
+        out_file.write(f'Total number of wrong detections found FP={nr_FP}\n')
+        out_file.write(f'Number of non-detected masses FN={nr_FN}\n')
+
+        # leggo tutti i GT e li metto in una lista,
+        # leggo tutte le predizioni e le metto in una lista
+        # per ogni predizione dovrei calcolare le distanze da ogni GT
 
 
 if __name__ == "__main__":
